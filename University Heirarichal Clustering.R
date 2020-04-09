27f7c4d902e8c54219af6ae679a281b04b1f7e9a
JanviDattani <62697710+JanviDattani@users.noreply.github.com>
2020-04-09 15:12:10 +0530

    Add files via upload

---
+++ University Heirarichal Clustering.R
@@ -0,0 +1,12 @@
+uni <- read.csv("~/Downloads/Universities.csv")
+View(uni)
+################################
+mydata <- scale(uni[,2:7])
+d <- dist(uni, method = "euclidean") #Computing the distance natrix
+fit <- hclust(d, method="average") # Building the algorithm # try with 'centroid'
+plot(fit) # display dendogram
+groups <- cutree(fit, k=4) # cut tree into 4 clusters
+# draw dendogram with red borders around the 5 clusters 
+rect.hclust(fit, k=4, border="red")
+#Attach the cluster numbers to Uni
+clusters=data.frame('Uni'=uni[,1],'Cluster' =groups)
